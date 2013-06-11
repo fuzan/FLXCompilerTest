acad4db99c6206274f33050062c07082af1dbbbc
rn4ru <mail@rn4ru.com>
2013-06-11 13:29:41 -0500

    clean up and start over

--- src/a/DomainEvent.java
+++
@@ -1,5 +0,0 @@
-package a;
-import external.*;
-import ddt.lang.*;import java.io.Serializable;
-public class DomainEvent extends FL_EVENT_STEM implements Serializable{
-}
--- src/a/Init.java
+++
@@ -1,6 +0,0 @@
-package a;import java.io.Serializable;
-
-public class Init extends DomainEvent implements Serializable{
-public Init() {}
-
-}
