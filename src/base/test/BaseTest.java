e2c39159257e86241577c4a61e192fe93321d5b7
rn4ru <mail@rn4ru.com>
2013-06-12 07:22:05 -0500

    rearrange methods

--- src/base/test/BaseTest.java
+++ src/base/test/BaseTest.java
@@ -19,7 +19,41 @@ public class BaseTest {
 		CompileSetting.compileCalc();
 	}
 
-	
+	@Test
+	public void testAddingOnce() {
+		SyncTool st = new SyncTool();
+		CalcFeature f = new CalcFeature(st);
+		st.starts();
+		f.sendEvent(new Add(10));
+		f.sendEvent(new Exit());
+		while (!st.isFinished()) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		assertEquals(10, st.getResult());
+	}
+
+	@Test
+	public void testAddingTwice() {
+		SyncTool st = new SyncTool();
+		CalcFeature f = new CalcFeature(st);
+		st.starts();
+		f.sendEvent(new Add(10));
+		f.sendEvent(new Add(10));
+		f.sendEvent(new Exit());
+		while (!st.isFinished()) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		assertEquals(20, st.getResult());
+	}
+
 	@Test
 	public void testAdding1to10() {
 		SyncTool st = new SyncTool();
@@ -29,50 +63,13 @@ public class BaseTest {
 			f.sendEvent(new Add(i));
 		}
 		f.sendEvent(new Exit());
-		while(!st.isFinished()){
+		while (!st.isFinished()) {
 			try {
 				Thread.sleep(1);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		assertEquals(55, st.getResult());		
-	}
-	
-	@Test
-	public void testAddingTwice() {
-		SyncTool st = new SyncTool();
-		CalcFeature f = new CalcFeature(st);
-		st.starts();
-		f.sendEvent(new Add(10));
-		f.sendEvent(new Add(10));
-		f.sendEvent(new Exit());
-		while(!st.isFinished()){
-			try {
-				Thread.sleep(1);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		assertEquals(20, st.getResult());
-	}
-	@Test
-	public void testAddingOnce() {
-		SyncTool st = new SyncTool();
-		CalcFeature f = new CalcFeature(st);
-		st.starts();
-		f.sendEvent(new Add(10));
-		f.sendEvent(new Exit());
-		while(!st.isFinished()){
-			try {
-				Thread.sleep(1);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		assertEquals(10, st.getResult());		
+		assertEquals(55, st.getResult());
 	}
 }
