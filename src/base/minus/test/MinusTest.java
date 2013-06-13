a33d0074ed21712aeeda274f22733f4dc9a6e7d1
rn4ru <mail@rn4ru.com>
2013-06-13 14:22:57 -0500

    add testcase

---
+++ src/base/minus/test/MinusTest.java
@@ -0,0 +1,43 @@
+package base.minus.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import base.minus.Minus;
+
+import base.Add;
+import base.Exit;
+import base.minus.Calc2Feature;
+
+import Resources.SyncTool;
+
+import compile.CompileSetting;
+
+public class MinusTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		CompileSetting.compileMinus();
+	}
+
+	@Test
+	public void testMinus() {
+		SyncTool st = new SyncTool();
+		Calc2Feature f = new Calc2Feature(st);
+		f.sendEvent(new Add(10));
+		f.sendEvent(new Minus(5));
+		f.sendEvent(new Exit());
+		while(!st.isFinished()){
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		assertEquals(5, st.getResult());
+	}
+
+}
